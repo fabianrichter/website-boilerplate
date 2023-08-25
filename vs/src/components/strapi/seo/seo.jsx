a5977439028876c82178f5903ea6cd8218e29bcd
Fabian Richter <fabian.richter@e-fork.de>
2023-08-25 23:42:26 +0200

    advanced seo meta tags

--- vs/src/components/strapi/seo/seo.jsx
+++ vs/src/components/strapi/seo/seo.jsx
@@ -1,3 +1,4 @@
+import { strapiUrl } from "@/config";
 import Head from "next/head";
 import React from "react";
 
@@ -7,11 +8,36 @@ import React from "react";
 const StrapiSEO = ({ data }) => {
   return (
     <Head>
+      {/* Basic meta tags */}
       <title>{data.metaTitle}</title>
       <meta property="og:title" content={data.metaTitle} key="title" />
       <meta name="description" content={data.metaDescription} key="description" />
       <meta name="keywords" content={data.keywords} key="keywords" />
       <meta name="robots" content={data.metaRobots} key="robots" />
+      {data.metaImage && (
+        <meta name="image" content={strapiUrl + data.metaImage.data.attributes.formats.medium.url} key="image" />
+      )}
+
+      {/* Facebook meta tags */}
+      {data.metaSocial &&
+        data.metaSocial.map((metaSocial) => {
+          switch (metaSocial.socialNetwork) {
+            case "Facebook":
+              return (
+                <>
+                  <meta name="og:title" content={metaSocial.title} key="og:title" />
+                  <meta name="og:description" content={metaSocial.description} key="og:description" />
+                </>
+              );
+            case "Twitter":
+              return (
+                <>
+                  <meta name="twitter:title" content={metaSocial.title} key="twitter:title" />
+                  <meta name="twitter:description" content={metaSocial.description} key="twitter:description" />
+                </>
+              );
+          }
+        })}
     </Head>
   );
 };
