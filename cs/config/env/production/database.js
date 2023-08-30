5796c56d6ac34515d40904425f308f2feb456c16
Fabian Richter <fabian.richter@e-fork.de>
2023-08-30 11:44:50 +0200

    updated production env variables

--- cs/config/env/production/database.js
+++ cs/config/env/production/database.js
@@ -1,18 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
+      host: "5432",
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_DATABASE"),
+      user: env("DATABASE_USER"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
