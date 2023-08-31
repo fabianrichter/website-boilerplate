86a0bd5d4c0954fd0c6f480e19519391e0aec97e
Fabian Richter <fabian.richter@e-fork.de>
2023-08-31 15:54:28 +0200

    mysql config

--- cs/config/env/production/database.js
+++ cs/config/env/production/database.js
@@ -3,6 +3,15 @@ const path = require("path");
 module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
+    pool: {
+      min: 0,
+      max: 10,
+      acquireTimeoutMillis: 100000,
+      createTimeoutMillis: 30000,
+      idleTimeoutMillis: 20000,
+      reapIntervalMillis: 20000,
+      createRetryIntervalMillis: 200,
+    },
     connection: {
       uri: env("DATABASE_URL"),
       host: env("DATABASE_HOST"),
@@ -10,6 +19,7 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_DATABASE"),
       user: env("DATABASE_USER"),
       password: env("DATABASE_PASSWORD"),
+      ssl: false
     },
     debug: false,
   },
