6a24100ac69013dff40887d1c8f3b31f4b9a2430
Fabian Richter <fabian.richter@e-fork.de>
2023-09-02 18:17:38 +0200

    postgresql setup

--- cs/config/env/production/database.js
+++ cs/config/env/production/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: "5432",
+      host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT"),
       database: env("DATABASE_DATABASE"),
       user: env("DATABASE_USER"),
