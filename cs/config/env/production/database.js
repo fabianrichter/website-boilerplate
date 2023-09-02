e11835576c8b9faf0c779dc8d294a936c0da537d
Fabian Richter <fabian.richter@e-fork.de>
2023-09-02 18:27:31 +0200

    merge preparation

--- cs/config/env/production/database.js
+++ cs/config/env/production/database.js
@@ -2,24 +2,16 @@ const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
-    pool: {
-      min: 0,
-      max: 10,
-      acquireTimeoutMillis: 100000,
-      createTimeoutMillis: 30000,
-      idleTimeoutMillis: 20000,
-      reapIntervalMillis: 20000,
-      createRetryIntervalMillis: 200,
-    },
+    client: "postgres",
     connection: {
-      uri: env("DATABASE_URL"),
       host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT"),
       database: env("DATABASE_DATABASE"),
       user: env("DATABASE_USER"),
       password: env("DATABASE_PASSWORD"),
-      ssl: false
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     debug: false,
   },
