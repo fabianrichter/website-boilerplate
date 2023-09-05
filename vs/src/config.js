31205e94e34176816176893e70c60a211575b714
Fabian Richter <fabian.richter@e-fork.de>
2023-09-05 19:13:44 +0200

    added nginx

--- vs/src/config.js
+++ vs/src/config.js
@@ -1,6 +1,6 @@
 const strapiUrl = process.env.CS_URL || "http://localhost:1337",
   graphqlEndpoint = strapiUrl + "/graphql",
-  strapiUrlServer = process.env.CS_URL_SERVER || "http://192.168.32.1:1337",
+  strapiUrlServer = "http://localhost:1337",
   graphqlEndpointServer = strapiUrlServer + "/graphql";
 
 export { strapiUrl, strapiUrlServer, graphqlEndpoint, graphqlEndpointServer };
