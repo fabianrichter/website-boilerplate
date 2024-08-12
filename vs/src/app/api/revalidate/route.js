54f89a1c6a1144d235a7e3b6e7397c2f5ddb79e5
Fabian Richter <fabian.richter@e-fork.de>
2024-08-12 15:54:25 +0200

    updated revalidation to match app router

--- vs/src/app/api/revalidate/route.js
+++ vs/src/app/api/revalidate/route.js
@@ -1,9 +1,13 @@
 import { apolloClient } from "@/app/apollo-client";
 import { PageSlugs, ArticleSlugs } from "@/queries/slugs.gql";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
 
-export default async function handler(req, res) {
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const secret = searchParams.get("secret");
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== "Qgf4tXL46N7EHiHbBCRxHTeQJ6mSrEiSixM5rY93") {
+  if (secret !== "Qgf4tXL46N7EHiHbBCRxHTeQJ6mSrEiSixM5rY93") {
     return res.status(401).json({ message: "Invalid token" });
   }
 
@@ -11,9 +15,9 @@ export default async function handler(req, res) {
     const client = apolloClient;
 
     // request all article and page slugs
-    const { data: articleData } = await client.query({
+    /* const { data: articleData } = await client.query({
       query: ArticleSlugs,
-    });
+    }); */
     const { data: pageData } = await client.query({
       query: PageSlugs,
     });
@@ -21,15 +25,15 @@ export default async function handler(req, res) {
     // gather the result in one array
     const paths = [
       ...pageData.pages.data.map((page) => "/" + page.attributes.slug),
-      ...articleData.articles.data.map((page) => "/articles/" + page.attributes.slug),
+      //...articleData.articles.data.map((page) => "/articles/" + page.attributes.slug),
     ];
 
     // pass paths into revalidate function
-    await Promise.all(paths.map(async (path) => await res.revalidate(path)));
-    return res.json({ revalidated: true, paths });
+    await Promise.all(paths.map((path) => revalidatePath(path)));
+    return NextResponse.json({ message: "Revalidation successful", paths }, { status: 200 });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send("Error revalidating");
+    return NextResponse.json({ message: "Error revalidating", err }, { status: 500 });
   }
 }
